ce0dce704b1ec204ce3b426451e5ffe2a1721f68
aero <aeroluxx@mail.ru>
2019-07-15 12:40:12 +0300

    platform divide pre-commit

--- components/Chart.web.js
+++ components/Chart.web.js
@@ -1,18 +1,16 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { View } from 'react-native'
 import { VictoryAxis, VictoryChart, VictoryTheme, VictoryCandlestick } from 'victory'
 import { DATA } from '../DATA'
 
-export default class PieChart extends Component {
-  render() {
-    return (
-      <View style={{ backgroundColor: 'light-grey', flex: 1 }}>
-        <VictoryChart theme={VictoryTheme.material} domainPadding={{ x: 25 }} scale={{ x: 'time' }}>
-          <VictoryAxis tickFormat={t => `${t.getDate()}/${t.getMonth()}`} />
-          <VictoryAxis dependentAxis />
-          <VictoryCandlestick candleColors={{ positive: 'green', negative: 'red' }} data={DATA} />
-        </VictoryChart>
-      </View>
-    )
-  }
-}
+const Chart = () => (
+  <View style={{ backgroundColor: 'light-grey', flex: 1 }}>
+    <VictoryChart theme={VictoryTheme.material} domainPadding={{ x: 25 }} scale={{ x: 'time' }}>
+      <VictoryAxis tickFormat={t => `${t.getDate()}/${t.getMonth()}`} />
+      <VictoryAxis dependentAxis />
+      <VictoryCandlestick candleColors={{ positive: 'green', negative: 'red' }} data={DATA} />
+    </VictoryChart>
+  </View>
+)
+
+export { Chart }
