d446dbbd7580ce76a28294e1a83338f5f3202947
aero <aeroluxx@mail.ru>
2019-07-15 10:54:39 +0300

    candle-stick-data

--- DATA.js
+++ DATA.js
@@ -1,18 +1,22 @@
-import React, { Component } from 'react'
-import { View } from 'react-native'
-import { VictoryAxis, VictoryChart, VictoryTheme, VictoryCandlestick } from 'victory-native'
-import { DATA } from './DATA'
-
-export default class PieChart extends Component {
-  render() {
-    return (
-      <View style={{ backgroundColor: 'light-grey', flex: 1 }}>
-        <VictoryChart theme={VictoryTheme.material} domainPadding={{ x: 25 }} scale={{ x: 'time' }}>
-          <VictoryAxis tickFormat={t => `${t.getDate()}/${t.getMonth()}`} />
-          <VictoryAxis dependentAxis />
-          <VictoryCandlestick candleColors={{ positive: '#5f5c5b', negative: '#c43a31' }} data={DATA} />
-        </VictoryChart>
-      </View>
-    )
-  }
-}
+export const DATA = [
+  { x: new Date(2016, 6, 1), open: 5, close: 10, high: 15, low: 1 },
+  { x: new Date(2016, 6, 2), open: 10, close: 15, high: 5, low: 5 },
+  { x: new Date(2016, 6, 3), open: 15, close: 20, high: 22, low: 10 },
+  { x: new Date(2016, 6, 4), open: 20, close: 10, high: 25, low: 7 },
+  { x: new Date(2016, 6, 5), open: 10, close: 8, high: 15, low: 5 },
+  { x: new Date(2016, 6, 6), open: 10, close: 15, high: 3, low: 5 },
+  { x: new Date(2016, 6, 7), open: 15, close: 20, high: 22, low: 13 },
+  { x: new Date(2016, 6, 8), open: 20, close: 10, high: 18, low: 7 },
+  { x: new Date(2016, 6, 9), open: 5, close: 10, high: 15, low: 5 },
+  { x: new Date(2016, 6, 10), open: 10, close: 15, high: 11, low: 7 },
+  { x: new Date(2016, 6, 11), open: 15, close: 20, high: 22, low: 10 },
+  { x: new Date(2016, 6, 12), open: 20, close: 10, high: 27, low: 9 },
+  { x: new Date(2016, 6, 14), open: 10, close: 8, high: 15, low: 5 },
+  { x: new Date(2016, 6, 15), open: 10, close: 15, high: 9, low: 1 },
+  { x: new Date(2016, 6, 16), open: 20, close: 10, high: 25, low: 7 },
+  { x: new Date(2016, 6, 18), open: 5, close: 10, high: 11, low: 2 },
+  { x: new Date(2016, 6, 20), open: 10, close: 15, high: 4, low: 5 },
+  { x: new Date(2016, 6, 21), open: 15, close: 20, high: 2, low: 10 },
+  { x: new Date(2016, 6, 22), open: 20, close: 10, high: 25, low: 7 },
+  { x: new Date(2016, 6, 23), open: 10, close: 8, high: 15, low: 5 }
+]
