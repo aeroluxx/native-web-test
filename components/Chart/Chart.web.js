1af0f9ccfa93b2c3be8a0f6ae52c3a962499e8c7
aero <aeroluxx@mail.ru>
2019-07-19 14:41:07 +0300

    styling

--- components/Chart/Chart.web.js
+++ components/Chart/Chart.web.js
@@ -1,16 +1,35 @@
 import React from 'react'
 import { View } from 'react-native'
-import { VictoryAxis, VictoryChart, VictoryTheme, VictoryCandlestick } from 'victory'
+import { VictoryAxis, VictoryChart, VictoryCandlestick } from 'victory'
 import { DATA } from '../../DATA'
 
 const Chart = () => (
   <View style={{ flex: 1 }}>
     <VictoryChart
       scale={{ x: 'time' }}
-      height={200}
-      maxDomain={{ y: 25 }}
-      padding={{ top: 10, bottom: 10, left: 20, right: 20 }}
+      height={230}
+      //width={900}
+      //maxDomain={{ y: 25 }}
+      //padding={{ top: 30, bottom: 10, left: 10, right: 20 }}
+      //margin={{ top: -100, bottom: 0, left: 0, right: 0 }}
+      domainPadding={{ x: 15, y: 15 }}
     >
+      <VictoryAxis
+        style={{
+          axis: { stroke: 'white' },
+          ticks: { fill: 'white' },
+          tickLabels: { fill: 'white' }
+        }}
+        fixLabelOverlap
+      />
+      <VictoryAxis
+        style={{
+          axis: { stroke: 'white' },
+          ticks: { fill: 'transparent' },
+          tickLabels: { fill: 'transparent' }
+        }}
+        dependentAxis
+      />
       <VictoryCandlestick
         candleColors={{ positive: 'green', negative: 'red' }}
         candleRatio={0.6}
@@ -20,17 +39,20 @@ const Chart = () => (
         height={250}
         data={DATA}
         //labels={d => `open: ${d.open}`}
-        padding={{ top: 20, bottom: 20, left: 25, right: 25 }}
-        //scale={{ x: 'linear', y: 'linear' }}
-        // style={{
-        //   data: {
-        //     fillOpacity: 0.7
-        //   },
-        //   labels: {
-        //     fontSize: 6,
-        //     fill: 'white'
-        //   }
-        // }}
+        padding={{ top: 0, bottom: 0, left: 5, right: 5 }}
+        scale={{ x: 'linear', y: 'linear' }}
+        style={{
+          data: {
+            //     fillOpacity: 0.7
+            //   },
+            stroke: 'white',
+            strokeWidth: 0.2
+            // labels: {
+            //   fontSize: 6,
+            //   fill: 'white'
+            // }
+          }
+        }}
         wickStrokeWidth={1}
       />
     </VictoryChart>
